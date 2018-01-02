7750ada9bc64833d936e40ebc6e053cbee14f6fc
Bruce Hem <bruce.hem@pathmazing.com>
2018-01-02 19:09:43 +0700

    clean up

--- test/index.ts
+++ test/index.ts
@@ -5,8 +5,6 @@ import 'source-map-support/register';
 import { ApplicationModule } from '../src/app/app.module';
 import { Startup } from '../src/lib/startup';
 
-(global as any).__TEST_SERVER__;
-
 export async function getServer() {
   if (!(global as any).__TEST_SERVER__) {
     const { app, server } = await new Startup({ ApplicationModule }).main();
--- test/mocks.ts
+++ test/mocks.ts
@@ -1,5 +1,5 @@
 import { Roles } from '../src/common';
-import { User, Role, Profile } from "../src/entity";
+import { Profile, Role, User } from '../src/entity';
 
 export const userRole = new Role();
 userRole.id = 3;
@@ -8,7 +8,7 @@ userRole.name = Roles.User;
 export const jamesBondProfile = new Profile();
 jamesBondProfile.id = 1;
 jamesBondProfile.lastName = 'bond';
-jamesBondProfile.firstName =  'james';
+jamesBondProfile.firstName = 'james';
 
 export const jamesBond = new User();
 jamesBond.id = 1;
@@ -16,4 +16,3 @@ jamesBond.email = 'jamebond@test.com';
 jamesBond.role = userRole;
 jamesBond.password = '123456';
 jamesBond.profile = jamesBondProfile;
-
