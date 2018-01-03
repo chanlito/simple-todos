bb2ac10e00e40c67c21d4507be16b12ee2e346db
Bruce Hem <bruce.hem@pathmazing.com>
2018-01-03 16:18:30 +0700

    fix tests

--- test/index.ts
+++ test/index.ts
@@ -3,11 +3,25 @@ import 'reflect-metadata';
 import 'source-map-support/register';
 
 import { ApplicationModule } from '../src/app/app.module';
+import { uniqueEmail } from '../src/common/rules/unique-email.rule';
 import { Startup } from '../src/lib/startup';
 
 export async function getServer() {
   if (!(global as any).__TEST_SERVER__) {
-    const { app, server } = await new Startup({ ApplicationModule }).main();
+    const { app, server } = await new Startup({
+      ApplicationModule,
+      indicative: {
+        defaultMessages: {
+          email: 'The {{field}} field is not valid.',
+          integer: 'The {{field}} field must be an integer.',
+          required: 'The {{field}} field is required.',
+          string: 'The {{field}} field must be a string.'
+        },
+        customRules: {
+          uniqueEmail
+        }
+      }
+    }).main();
     await app.init();
     (global as any).__TEST_SERVER__ = server;
   }
