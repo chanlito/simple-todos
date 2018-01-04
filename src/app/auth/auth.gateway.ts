17c1272b4c4308808bcd3c8a0de21b36c7de43e5
Bruce Hem <bruce.hem@pathmazing.com>
2018-01-04 17:47:12 +0700

    Merge branch 'master' of github.com:BruceHem/simple-todos

--- src/app/auth/auth.gateway.ts
+++ src/app/auth/auth.gateway.ts
@@ -1,18 +1,18 @@
 import { Logger, UseFilters } from '@nestjs/common';
-import { OnGatewayConnection, OnGatewayDisconnect, WebSocketGateway } from '@nestjs/websockets';
+import { OnGatewayConnection, OnGatewayDisconnect, WebSocketGateway, WsException } from '@nestjs/websockets';
 
 import { WebsocketsExceptionFilter } from '../../common';
 import { AuthGatewayMiddleware } from './auth.gateway-middleware';
 
 @WebSocketGateway({ namespace: 'auth', middlewares: [AuthGatewayMiddleware] })
-@UseFilters(new WebsocketsExceptionFilter())
 export class AuthGateway implements OnGatewayConnection, OnGatewayDisconnect {
   private logger = new Logger('AuthGateway');
   // @WebSocketServer() private readonly io: SocketIO.Server;
 
-  handleConnection(socket: SocketIO.Socket) {
+  @UseFilters(new WebsocketsExceptionFilter())
+  async handleConnection(socket: SocketIO.Socket) {
     this.logger.log(`${socket.id} connected!`);
-    // throw new Error('WTF');
+    throw new WsException('WTF'); // <----- Like this one!
     // console.log(this.io);
   }
 
--- src/lib/ws/ws-exception.filter.ts
+++ src/lib/ws/ws-exception.filter.ts
@@ -1,9 +1,10 @@
 import { Catch, WsExceptionFilter } from '@nestjs/common';
 import { WsException } from '@nestjs/websockets';
 
-@Catch(Error, WsException)
+@Catch(WsException)
 export class WebsocketsExceptionFilter implements WsExceptionFilter {
   catch(exception: WsException, socket: SocketIO.Socket) {
+    console.log('exception', exception);
     socket.emit('exception', {
       status: 'error',
       message: `It's a message from the exception filter`
